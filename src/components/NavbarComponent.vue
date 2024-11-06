a5fa526297fedc84afe9374c97a2bff14379a3ab
GitLab CI <ci@noreply.gitlab.com>
2024-11-06 10:59:17 +0000

    Lint and format

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -17,59 +17,57 @@ const spanHeadingText = 'AI'
       </h3>
     </div>
 
-        
-
-        <div class="flex nav-switch gradient-green-dark">
-            <div class="nav-thumb"> </div>
-        </div>
-    </nav>
-
-
+    <div class="flex nav-switch gradient-green-dark">
+      <div class="nav-thumb"></div>
+    </div>
+  </nav>
 </template>
 
 <style>
+.fox-find-logo {
+  height: calc(var(--fs-h3) + 1rem); /*Größe abhängig von der h3 font-size*/
+  width: calc(var(--fs-h3) + 1rem);
+  object-fit: cover;
+  border-radius: 0px; /*Ausnahme für das Logo*/
+}
 
-    .fox-find-logo{
-        height:  calc(var(--fs-h3) + 1rem); /*Größe abhängig von der h3 font-size*/
-        width: calc(var(--fs-h3) + 1rem);
-        object-fit: cover;
-        border-radius: 0px; /*Ausnahme für das Logo*/
-    }
+nav {
+  background-color: var(--dark-bg-primary);
+  height: var(--navbar-height);
+  width: 98%;
+  padding: 0 1%;
+}
 
-    nav{
-        background-color: var(--dark-bg-primary);
-        height: var(--navbar-height);
-        width: 98%;
-        padding: 0 1%;
-    }
+.gradient-green-dark {
+  background: rgb(0, 176, 104);
+  background: linear-gradient(
+    -105deg,
+    rgba(0, 176, 104, 1) 0%,
+    rgba(0, 220, 128, 1) 100%
+  );
+}
 
-    .gradient-green-dark{
-        background: rgb(0,176,104);
-        background: linear-gradient(-105deg, rgba(0,176,104,1) 0%, rgba(0,220,128,1) 100%);
-    }
+.searchbar {
+  height: 2rem;
+  width: 30rem;
+  border-radius: var(--border-radius-full);
+}
 
-    .searchbar{
-        height: 2rem;
-        width: 30rem;
-        border-radius: var(--border-radius-full);
-    }
+.nav-switch {
+  align-items: center;
+  justify-content: end;
 
-    .nav-switch{
-        align-items: center;
-        justify-content: end;
-    
-        border-radius: var(--border-radius-full);
-        padding: 0 0.3rem;
-        height: calc(var(--fs-h3) - 0.5rem);
-        width:  calc(var(--fs-h3) + 0.5rem);
-    }
-    .nav-thumb{
-        background:var(--dark-primary) ;
-        border-radius: var(--border-radius-full);
-        
-        /* thumb size: 1.5rem * 1.5rem */
-        height: calc(var(--fs-h3) - 1rem); 
-        width: calc(var(--fs-h3) - 1rem);
-    }
+  border-radius: var(--border-radius-full);
+  padding: 0 0.3rem;
+  height: calc(var(--fs-h3) - 0.5rem);
+  width: calc(var(--fs-h3) + 0.5rem);
+}
+.nav-thumb {
+  background: var(--dark-primary);
+  border-radius: var(--border-radius-full);
 
+  /* thumb size: 1.5rem * 1.5rem */
+  height: calc(var(--fs-h3) - 1rem);
+  width: calc(var(--fs-h3) - 1rem);
+}
 </style>
