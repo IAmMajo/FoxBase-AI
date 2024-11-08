5b30e49bce6dc3cffba79e5720af969434e0c30b
GitLab CI <ci@noreply.gitlab.com>
2024-11-08 12:18:42 +0000

    Lint and format

--- components/ResultCardsComponent.vue
+++ components/ResultCardsComponent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 // import type { Product } from '../types/Product'
 
 // const product = defineProps<Product>()
@@ -18,7 +17,7 @@
 //   }
 // })
 
-import ResultCardComponent from './ResultCardComponent.vue';
+import ResultCardComponent from "./ResultCardComponent.vue";
 //import type Product from '../types/Product.d.ts';
 
 interface Product {
@@ -38,13 +37,13 @@ const products = ref<Product[]>([
     category: "Notebook",
     description: "A high-quality smartphone with advanced features.",
     attributes: {
-        Processor: "Apple M3 Chip", 
-        Memory: "16 GB",
-        Storage: "256 GB",
-        Display: "Liquid Retina Display"
+      Processor: "Apple M3 Chip",
+      Memory: "16 GB",
+      Storage: "256 GB",
+      Display: "Liquid Retina Display",
     },
     typicalUseCases: ["Remote work", "Business travel", "Office use"],
-    score: 1
+    score: 1,
   },
   {
     id: 2,
@@ -52,13 +51,13 @@ const products = ref<Product[]>([
     category: "Mini PC",
     description: "A powerful laptop for professionals and students.",
     attributes: {
-        Processor: "3,0 GHz 6-Core Intel Core i5", 
-        Memory: "8 GB",
-        Storage: "512 GB SSD",
-        GPU: "UHD 630"
+      Processor: "3,0 GHz 6-Core Intel Core i5",
+      Memory: "8 GB",
+      Storage: "512 GB SSD",
+      GPU: "UHD 630",
     },
     typicalUseCases: ["Productivity", "Portability", "Office use"],
-    score: 85
+    score: 85,
   },
   {
     id: 3,
@@ -72,47 +71,41 @@ const products = ref<Product[]>([
       connectivity: "Bluetooth 5.0",
     },
     typicalUseCases: ["Music", "Podcasts", "Video Calls"],
-    score: 88
-  }
+    score: 88,
+  },
 ]);
-
-
 </script>
 
-
 <template>
-    <div class="result-container flex flex-column jc-ai-center gap">
-        <ResultCardComponent 
-            v-for="(product, index) in products"
-            :key="product.id"
-            :index="index"
-            :name="product.name"
-            :category="product.category"
-            :description="product.description"
-            :attributes="product.attributes"
-            :typical-use-cases="product.typicalUseCases"
-            :score="product.score"
-        />
-    </div>
+  <div class="result-container flex flex-column jc-ai-center gap">
+    <ResultCardComponent
+      v-for="(product, index) in products"
+      :key="product.id"
+      :index="index"
+      :name="product.name"
+      :category="product.category"
+      :description="product.description"
+      :attributes="product.attributes"
+      :typical-use-cases="product.typicalUseCases"
+      :score="product.score"
+    />
+  </div>
 </template>
 
-
 <style>
-
 .result-container {
-    background-color: var(--dark-bg-primary);
+  background-color: var(--dark-bg-primary);
 }
 
 @media (max-width: 480px) {
-    .result-container {
-        padding-top: 20px;
-    }
+  .result-container {
+    padding-top: 20px;
+  }
 }
 
 @media (max-width: 400px) {
-    .result-container {
-        padding-top: 50px;
-    }
+  .result-container {
+    padding-top: 50px;
+  }
 }
-
 </style>
