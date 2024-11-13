a6fd5b7e9720c0eff05b5eb06055c8157a15276b
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-11-13 16:19:02 +0100

    added Text Media Component

---
+++ components/TextmediaComponent.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    titleHighlight: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    bottomText: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="text-media-container flex">
+    <div class="half-width flex jc-ai-center p-2vw">
+      <img class="full-size" :src="image" alt="Image">
+    </div>
+    <div class="half-width flex jc-start-ai-center p-2vw">
+      <div>
+        <h2 class="dark-heading">
+          {{ title }} <span class="dark-highlight">{{ titleHighlight }}</span>
+        </h2>
+        <p class="dark-bottomtext">{{ bottomText }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.text-media-container {
+  height: 90vh;
+  width: 100%;
+  background-color: var(--dark-bg-primary);
+}
+
+.text-media-container img {
+  height: 80%;
+  width: 80%;
+}
+</style>
