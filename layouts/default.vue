1a0cdfee1713cdd4efa760014666c32c56682142
GitLab CI <ci@noreply.gitlab.com>
2024-11-21 18:44:47 +0000

    Lint and format

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,151 +1,139 @@
 <!-- eslint-disable vue/no-multiple-template-root -->
 <template>
-    <Menubar>
-      <template #start>
-        <div class="hidden md:block">
-          <div class="flex flex-row flex-wrap">
-            <div class="w-15rem flex align-items-center flex-wrap">
-              <img
-                class="fox-find-logo"
-                src="./../assets/FoxFindLogo.svg"
-                alt="fox logo"
-                width="35"
-                heigt="40"
-                viewBox="0 0 35 40"
-              />
-              <span class="font-semibold text-2xl text-primary"
-                >FoxBase AI</span
-              >
-            </div>
+  <Menubar>
+    <template #start>
+      <div class="hidden md:block">
+        <div class="flex flex-row flex-wrap">
+          <div class="w-15rem flex align-items-center flex-wrap">
+            <img
+              class="fox-find-logo"
+              src="./../assets/FoxFindLogo.svg"
+              alt="fox logo"
+              width="35"
+              heigt="40"
+              viewBox="0 0 35 40"
+            />
+            <span class="font-semibold text-2xl text-primary">FoxBase AI</span>
+          </div>
 
-            <div class="">
-              <Button
-                icon="pi pi-bars"
-                severity="secondary"
-                aria-label="Bookmark"
-                text
-                @click="menuMain = !menuMain"
-              />
-            </div>
+          <div class="">
+            <Button
+              icon="pi pi-bars"
+              severity="secondary"
+              aria-label="Bookmark"
+              text
+              @click="menuMain = !menuMain"
+            />
           </div>
         </div>
+      </div>
 
-        <div class="block md:hidden">
-          <div class="grid">
-            <div class="col-fixed" style="width: 35px">
-              <Button
-                icon="pi pi-bars"
-                severity="secondary"
-                aria-label="Bookmark"
-                text
-                @click="menuMainMobile = !menuMainMobile"
-              />
-            </div>
+      <div class="block md:hidden">
+        <div class="grid">
+          <div class="col-fixed" style="width: 35px">
+            <Button
+              icon="pi pi-bars"
+              severity="secondary"
+              aria-label="Bookmark"
+              text
+              @click="menuMainMobile = !menuMainMobile"
+            />
+          </div>
 
-            <div class="col flex align-items-center justify-content-center">
-              <img
-                class="fox-find-logo"
-                src="./../assets/FoxFindLogo.svg"
-                alt="fox logo"
-                width="35"
-                heigt="40"
-                viewBox="0 0 35 40"
-              />
-              <span class="font-semibold text-2xl text-primary"
-                >FoxBase AI</span
-              >
-            </div>
+          <div class="col flex align-items-center justify-content-center">
+            <img
+              class="fox-find-logo"
+              src="./../assets/FoxFindLogo.svg"
+              alt="fox logo"
+              width="35"
+              heigt="40"
+              viewBox="0 0 35 40"
+            />
+            <span class="font-semibold text-2xl text-primary">FoxBase AI</span>
           </div>
         </div>
-      </template>
+      </div>
+    </template>
 
-      <template #end>
-        <div class="flex align-items-center gap-2">
-          <Avatar
-            image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
-            width="35"
-            heigt="40"
-            viewBox="0 0 35 40"
-            shape="circle"
-            aria-haspopup="true"
-            aria-controls="overlay_menu"
-            @click="toggleMenuProfile"
-          />
-          <Menu
-            id="overlay_menu"
-            ref="menuProfile"
-            :model="menuProfileItems"
-            :popup="true"
-          >
-            <template #item="{ item, props }">
-              <router-link
-                v-if="item.route"
-                v-slot="{ href, navigate }"
-                :to="item.route"
-                custom
-              >
-                <a
-                  v-ripple
-                  :href="href"
-                  v-bind="props.action"
-                  @click="navigate"
-                >
-                  <span :class="item.icon" />
-                  <span class="ml-2">{{ item.label }}</span>
-                </a>
-              </router-link>
-            </template>
-          </Menu>
-        </div>
-      </template>
-    </Menubar>
+    <template #end>
+      <div class="flex align-items-center gap-2">
+        <Avatar
+          image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png"
+          width="35"
+          heigt="40"
+          viewBox="0 0 35 40"
+          shape="circle"
+          aria-haspopup="true"
+          aria-controls="overlay_menu"
+          @click="toggleMenuProfile"
+        />
+        <Menu
+          id="overlay_menu"
+          ref="menuProfile"
+          :model="menuProfileItems"
+          :popup="true"
+        >
+          <template #item="{ item, props }">
+            <router-link
+              v-if="item.route"
+              v-slot="{ href, navigate }"
+              :to="item.route"
+              custom
+            >
+              <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                <span :class="item.icon" />
+                <span class="ml-2">{{ item.label }}</span>
+              </a>
+            </router-link>
+          </template>
+        </Menu>
+      </div>
+    </template>
+  </Menubar>
 
-    <Sidebar
-      v-model:visible="menuMain"
-      :modal="false"
-      :dismissable="false"
-      :base-z-index="100"
-    >
+  <Sidebar
+    v-model:visible="menuMain"
+    :modal="false"
+    :dismissable="false"
+    :base-z-index="100"
+  >
+    <div class="flex flex-column h-full">
+      <mainMenu />
+    </div>
+  </Sidebar>
+
+  <Sidebar v-model:visible="menuMainMobile" :base-z-index="1001">
+    <template #container="{ closeCallback }">
       <div class="flex flex-column h-full">
+        <div
+          class="flex align-items-center justify-content-between px-4 pt-3 flex-shrink-0"
+        >
+          <span class="inline-flex align-items-center gap-2">
+            <img
+              class="fox-find-logo"
+              src="./../assets/FoxFindLogo.svg"
+              alt="fox logo"
+              width="35"
+              heigt="40"
+              viewBox="0 0 35 40"
+            />
+            <span class="font-semibold text-2xl text-primary">Foxbase AI</span>
+          </span>
+          <span>
+            <Button
+              type="button"
+              icon="pi pi-times"
+              rounded
+              outlined
+              class="h-2rem w-2rem"
+              @click="closeCallback"
+            ></Button>
+          </span>
+        </div>
         <mainMenu />
       </div>
-    </Sidebar>
-
-    <Sidebar v-model:visible="menuMainMobile" :base-z-index="1001">
-      <template #container="{ closeCallback }">
-        <div class="flex flex-column h-full">
-          <div
-            class="flex align-items-center justify-content-between px-4 pt-3 flex-shrink-0"
-          >
-            <span class="inline-flex align-items-center gap-2">
-              <img
-                class="fox-find-logo"
-                src="./../assets/FoxFindLogo.svg"
-                alt="fox logo"
-                width="35"
-                heigt="40"
-                viewBox="0 0 35 40"
-              />
-              <span class="font-semibold text-2xl text-primary"
-                >Foxbase AI</span
-              >
-            </span>
-            <span>
-              <Button
-                type="button"
-                icon="pi pi-times"
-                rounded
-                outlined
-                class="h-2rem w-2rem"
-                @click="closeCallback"
-              ></Button>
-            </span>
-          </div>
-          <mainMenu />
-        </div>
-      </template>
-    </Sidebar>
-
+    </template>
+  </Sidebar>
 
   <div
     class="py-6 pr-3 md:pr-6"
