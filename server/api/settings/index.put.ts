85709127c691d2d68740086266ce239ea1c14b41
Majo <git@maxoverlack.dev>
2024-11-21 22:23:19 +0100

    #52 Finish initial database integration

--- server/api/settings/index.put.ts
+++ server/api/settings/index.put.ts
@@ -1,18 +1,30 @@
-import { z } from "zod";
+/*
+ * Don't uncomment until auth is added. Not everyone should be able to insert
+ * settings into the database.
+ */
 
-const settingSchema = z.object({
-  name: z.string(),
-  value: z.string(),
-});
+/*
+ * import { z } from "zod";
+ *
+ * const settingSchema = z.object({
+ *   name: z.string(),
+ *   value: z.string(),
+ * });
+ */
 
-export default defineEventHandler(async (event) => {
-  const body = await readValidatedBody(event, (body) =>
-    settingSchema.parse(body),
-  );
-  const db = useDatabase();
-  const result = await db.sql<DbExecResult>`
-    INSERT INTO settings (name, value) VALUES (${body.name}, ${body.value})
-    ON CONFLICT (name) DO UPDATE SET value = ${body.value}
-  `;
-  return result.success;
+export default defineEventHandler(async (/* event */) => {
+  /*
+   * const body = await readValidatedBody(event, (body) =>
+   *   settingSchema.parse(body),
+   * );
+   * 
+   * const db = useDatabase();
+   * const result = await db.sql<DbExecResult>`
+   *   INSERT INTO settings (name, value) VALUES (${body.name}, ${body.value})
+   *   ON CONFLICT (name) DO UPDATE SET value = ${body.value}
+   * `;
+   * if (!result.success) {
+   *   throw createError("Something went wrong during database operation");
+   * }
+   */
 });
