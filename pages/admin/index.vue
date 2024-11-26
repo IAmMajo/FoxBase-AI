a04bc09499a456d0fa73557dc66e79780b592ca6
GitLab CI <ci@noreply.gitlab.com>
2024-11-26 13:54:46 +0000

    Lint and format

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,5 @@
 <script>
-
-  import CardComponent from "./components/CardComponent.vue"
-
+import CardComponent from "./components/CardComponent.vue";
 
 export default {
   components: {
@@ -13,33 +11,32 @@ export default {
 <template>
   <div class="flex flex-wrap gap">
     <CardComponent
-    title="Orders"
-    value=125
-    bottomValue = 24
-    bottomText = " new since last visit"
-    icon="pi-shopping-cart"
+      title="Orders"
+      value="125"
+      bottom-value="24"
+      bottom-text=" new since last visit"
+      icon="pi-shopping-cart"
     />
     <CardComponent
-    title="Revenue"
-    value=2.100€
-    bottomValue = 52%
-    bottomText = " more since last week"
-    icon="pi-dollar"
+      title="Revenue"
+      value="2.100€"
+      bottom-value="52%"
+      bottom-text=" more since last week"
+      icon="pi-dollar"
     />
     <CardComponent
-    title="Customers"
-    value=28441
-    bottomValue = 520
-    bottomText = " new registrations"
-    icon="pi-users"
+      title="Customers"
+      value="28441"
+      bottom-value="520"
+      bottom-text=" new registrations"
+      icon="pi-users"
     />
     <CardComponent
-    title="Comments"
-    value="152 unread"
-    bottomValue = 12
-    bottomText = " new threads"
-    icon="pi-comment"
+      title="Comments"
+      value="152 unread"
+      bottom-value="12"
+      bottom-text=" new threads"
+      icon="pi-comment"
     />
-    
   </div>
 </template>
