0821bc73a0a55575284d6579d0ebb8d7fe636e1b
GitLab CI <ci@noreply.gitlab.com>
2024-12-03 10:49:12 +0000

    Lint and format

--- pages/admin/konfiguration/index.vue
+++ pages/admin/konfiguration/index.vue
@@ -15,93 +15,69 @@ export default {
   <div>
     <h2>Konfiguration</h2>
     <p>
-      Auf dieser Seite können Konfigurationen über das Aussehen und Verhalten der
-      Seite festgelegt werden.
+      Auf dieser Seite können Konfigurationen über das Aussehen und Verhalten
+      der Seite festgelegt werden.
     </p>
 
     <h3>Preview</h3>
 
-
     <div class="flex gap">
-
-
       <div
-      class="preview-container flex flex-column jc-ai-center"
-      :style="{ backgroundColor: backgroundSecondary }"
-    >
-      <h2 :style="{ color: textPrimary }">Das ist der Titel</h2>
-      <p :style="{ color: textSecondary }">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sequi
-        eligendi modi minus veritatis quasi. Est veniam at laborum, excepturi
-        eum natus dolorum velit repudiandae. Laudantium quas provident ab
-        facilis quos culpa, impedit deleniti atque cupiditate eos iure, totam
-        officia reprehenderit vitae sint odio quis qui aut pariatur consequatur
-        placeat consequuntur, harum facere inventore. Ullam nihil unde corrupti
-        quidem quo.
-      </p>
-      <div class="flex jc-ai-center gap">
-        <button
-          class="button-preview rounded"
-          :style="{ backgroundColor: primary }"
-        >
-          Das ist ein runder Button
-        </button>
-        <button class="button-preview" :style="{ backgroundColor: primary }">
-          Das ist ein eckiger Button
-        </button>
-      </div>
-    </div>
-
-
-    <div class="flex flex-column" style="width: 22%;">
-      <h5>Farbpalette</h5>
-      <div class="flex jc-around-ai-center gap flex-wrap">
-        <div class="flex flex-column jc-ai-center color-field">
-          <input
-          type="color"
-          id="primary"
-          v-model="primary"
-          />
-          <label for="primary">Primary</label>
-        </div>
-  
-        <div class="flex flex-column jc-ai-center color-field">
-          <input
-          type="color"
-          id="backgroundSecondary"
-          v-model="backgroundSecondary"
-          />
-          <label for="backgroundSecondary">Hintergrund</label>
-        </div>
-  
-        <div class="flex flex-column jc-ai-center color-field">
-          <input
-          type="color"
-          id="textPrimary"
-          v-model="textPrimary"
-          />
-          <label for="textPrimary">Überschriften</label>
-        </div>
-  
-        <div class="flex flex-column jc-ai-center color-field">
-          <input
-          type="color"
-          id="textSecondary"
-          v-model="textSecondary"
-          />
-          <label for="textSecondary">Texte</label>
+        class="preview-container flex flex-column jc-ai-center"
+        :style="{ backgroundColor: backgroundSecondary }"
+      >
+        <h2 :style="{ color: textPrimary }">Das ist der Titel</h2>
+        <p :style="{ color: textSecondary }">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sequi
+          eligendi modi minus veritatis quasi. Est veniam at laborum, excepturi
+          eum natus dolorum velit repudiandae. Laudantium quas provident ab
+          facilis quos culpa, impedit deleniti atque cupiditate eos iure, totam
+          officia reprehenderit vitae sint odio quis qui aut pariatur
+          consequatur placeat consequuntur, harum facere inventore. Ullam nihil
+          unde corrupti quidem quo.
+        </p>
+        <div class="flex jc-ai-center gap">
+          <button
+            class="button-preview rounded"
+            :style="{ backgroundColor: primary }"
+          >
+            Das ist ein runder Button
+          </button>
+          <button class="button-preview" :style="{ backgroundColor: primary }">
+            Das ist ein eckiger Button
+          </button>
         </div>
       </div>
 
+      <div class="flex flex-column" style="width: 22%">
+        <h5>Farbpalette</h5>
+        <div class="flex jc-around-ai-center gap flex-wrap">
+          <div class="flex flex-column jc-ai-center color-field">
+            <input id="primary" v-model="primary" type="color" />
+            <label for="primary">Primary</label>
+          </div>
+
+          <div class="flex flex-column jc-ai-center color-field">
+            <input
+              id="backgroundSecondary"
+              v-model="backgroundSecondary"
+              type="color"
+            />
+            <label for="backgroundSecondary">Hintergrund</label>
+          </div>
+
+          <div class="flex flex-column jc-ai-center color-field">
+            <input id="textPrimary" v-model="textPrimary" type="color" />
+            <label for="textPrimary">Überschriften</label>
+          </div>
+
+          <div class="flex flex-column jc-ai-center color-field">
+            <input id="textSecondary" v-model="textSecondary" type="color" />
+            <label for="textSecondary">Texte</label>
+          </div>
+        </div>
+      </div>
     </div>
-
-    </div>
-
-
-
-    
-
-    
   </div>
 </template>
 
@@ -137,7 +113,7 @@ export default {
   gap: 5px;
 }
 
-.color-field label{
+.color-field label {
   font-size: 16px;
 }
 
