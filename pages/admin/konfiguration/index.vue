b66d47d22b32a30a8baf6c3f244d5e14f977f9d0
SetsunaiCodes <nicojin.m0sic@hotmail.com>
2024-12-16 15:58:24 +0100

    added confirmation

--- pages/admin/konfiguration/index.vue
+++ pages/admin/konfiguration/index.vue
@@ -7,6 +7,8 @@ const backgroundSecondary = ref(
 const textPrimary = ref(data.value?.colorTextPrimary || "#FFFFFF");
 const textSecondary = ref(data.value?.colorTextSecondary || "#CBD5E1");
 
+const saveStatus = ref<"sucess" | "error" | null>(null);
+
 async function onSave() {
   const success = await patchSettings({
     colorPrimary: primary.value,
@@ -14,6 +16,15 @@ async function onSave() {
     colorTextPrimary: textPrimary.value,
     colorTextSecondary: textSecondary.value,
   });
+
+  saveStatus.value = success ? "sucess" : "error";
+
+  setTimeout(() => {
+    saveStatus.value = null;
+  }, 3000);
+
+
+
   if (success) {
     console.log("Speichern erfolgreich");
   } else {
@@ -34,29 +45,44 @@ async function onSave() {
 
     <div class="flex gap">
       <div
-        class="preview-container flex flex-column jc-ai-center"
+        class="preview-container flex flex-column jc-start-ai-center relative z-1"
         :style="{ backgroundColor: backgroundSecondary }"
       >
-        <h2 :style="{ color: textPrimary }">Das ist der Titel</h2>
-        <p :style="{ color: textSecondary }">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sequi
-          eligendi modi minus veritatis quasi. Est veniam at laborum, excepturi
-          eum natus dolorum velit repudiandae. Laudantium quas provident ab
-          facilis quos culpa, impedit deleniti atque cupiditate eos iure, totam
-          officia reprehenderit vitae sint odio quis qui aut pariatur
-          consequatur placeat consequuntur, harum facere inventore. Ullam nihil
-          unde corrupti quidem quo.
-        </p>
-        <div class="flex jc-ai-center gap">
-          <button
-            class="button-preview rounded"
-            :style="{ backgroundColor: primary }"
-          >
-            Das ist ein runder Button
-          </button>
-          <button class="button-preview" :style="{ backgroundColor: primary }">
-            Das ist ein eckiger Button
-          </button>
+        <nav class="relative flex jc-space-between-ai-center relative t-0 px-4">
+          <div class="darkening-layer-secondary absolute full-size-percent"></div>
+         
+         
+          <div>
+            <div class="flex" >
+              <h5 :style="{color: textPrimary}">FoxSearch</h5>
+              <h5 :style="{color: primary}">AI</h5>            
+            </div>
+          </div>
+          <button class="flex jc-ai-center" :style="{backgroundColor: primary, height: '50%'}" > <p>Test</p></button>
+        </nav>
+
+        <div class="nav-preview-content-container flex flex-column jc-ai-center">
+          <h2 :style="{ color: textPrimary }">Das ist der Titel</h2>
+          <p :style="{ color: textSecondary }">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, sequi
+            eligendi modi minus veritatis quasi. Est veniam at laborum, excepturi
+            eum natus dolorum velit repudiandae. Laudantium quas provident ab
+            facilis quos culpa, impedit deleniti atque cupiditate eos iure, totam
+            officia reprehenderit vitae sint odio quis qui aut pariatur
+            consequatur placeat consequuntur, harum facere inventore. Ullam nihil
+            unde corrupti quidem quo.
+          </p>
+          <div class="flex jc-ai-center gap">
+            <button
+              class="button-preview rounded"
+              :style="{ backgroundColor: primary }"
+            >
+              Das ist ein runder Button
+            </button>
+            <button class="button-preview" :style="{ backgroundColor: primary }">
+              Das ist ein eckiger Button
+            </button>
+        </div>
         </div>
       </div>
 
@@ -90,12 +116,84 @@ async function onSave() {
         <div class="flex jc-ai-center w-100 mt">
           <button class="p-button bold-text" @click="onSave">Speichern</button>
         </div>
+
+        <div class="confirm-container mt">
+
+          <div v-if="saveStatus === 'sucess'" data-aos="fade-up" class="confirm-green flex jc-ai-center">
+            <div class="icon-container flex jc-ai-center">
+              <div class="green-icon pi pi-check text-xl"></div>
+            </div>
+            <p class="flex-grow text-center">Speichern erfolgreich</p>
+          </div>
+
+          <div v-if="saveStatus === 'error'" data-aos="fade-up" class="confirm-red flex jc-ai-center">
+            <div class="icon-container-red flex jc-ai-center">
+              <div class="red-icon pi pi-times text-xl"></div>
+            </div>
+            <p class="flex-grow text-center">Speichern fehlgeschlagen</p>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+
+.dot{
+  height: 10px;  
+  width: 10px;
+}
+
+.green-icon{
+  color: #00dc82;
+}
+.red-icon{
+  color: #dc0025;
+}
+
+.icon-container-red {
+  width: 3rem;
+  height: 3rem;
+  background-color: #dc002520;
+  border-radius: 8px;
+}
+
+.confirm-green{
+  background-color: #00dc802c;
+  height: 50%;
+  width: 100%;
+  border: 1px solid #00dc80;
+  padding: 14px 12px;
+  border-radius: var(--border-radius-default);
+}
+
+.confirm-red{
+  background-color: #dc00252c;
+  height: 50%;
+  width: 100%;
+  border: 1px solid #dc0025;
+  padding: 14px 12px;
+  border-radius: var(--border-radius-default);
+}
+
+.confirm-container{
+  height: 15vh;
+  width: 100%;
+}
+
+
+.nav-preview-content-container{
+  flex-grow: 1; 
+  text-align: center;
+}
+
+.nav-preview-content-container p{
+  width: 80%;
+}
+
+
 .button-preview {
   margin-top: 20px;
   padding: 12px 20px;
@@ -114,9 +212,22 @@ async function onSave() {
 .preview-container {
   height: 70vh;
   width: 75%;
-  padding: 0 10%;
   border-radius: var(--border-radius-default);
   box-shadow: var(--box-shadow);
+}
+
+.preview-container nav{
+  height: 10%;
+  background-color: var(--dark-bg-seconndary);
+}
+
+.preview-container nav button{
+  height: 60%;
+  padding: 0 22px;
+  border-radius: 6px;
+  border: none;
+  font-weight: 500;
+  cursor: pointer;
 }
 
 .color-field {
