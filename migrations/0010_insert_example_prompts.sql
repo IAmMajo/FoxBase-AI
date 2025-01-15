1ccad2b5b8f55ecb54c0bfa94b7c873e04d1f135
Majo <git@maxoverlack.dev>
2025-01-15 03:09:50 +0100

    Add migrations to insert example prompts

---
+++ migrations/0010_insert_example_prompts.sql
@@ -0,0 +1,23 @@
+-- Migration number: 0010 	 2025-01-15T02:06:32.504Z
+
+INSERT INTO prompts (text, user) VALUES
+  ('Laptop with long battery life', 1),
+  ('Fast laptop', 1),
+  ('Lightweight laptop', 1),
+  ('Light laptop', 1),
+  ('High-performance laptop', 1),
+  ('Laptop with good display', 1),
+  ('Laptop with large storage', 1),
+  ('Student laptop', 1),
+  ('Student notebook', 1),
+  ('Smartphone with good price-performance ratio', 1),
+  ('Tablet with pen', 1),
+  ('Tablet with large display', 1),
+  ('5G smartphone', 1),
+  ('Compact smartphone', 1),
+  ('Smartphone with long battery life', 1),
+  ('Smartphone accessories', 1),
+  ('Laptop accessories', 1),
+  ('Computer accessories', 1),
+  ('Wireless phone charger', 1),
+  ('Smartphone power bank', 1);
